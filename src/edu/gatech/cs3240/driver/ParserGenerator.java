2ec52d26b5bd857e6adc006737261fd8e573d108
swgillespie <sean.william.g@gmail.com>
2013-02-21 15:19:15 -0500

    Add eclipse project files

--- src/edu/gatech/cs3240/driver/ParserGenerator.java
+++ src/edu/gatech/cs3240/driver/ParserGenerator.java
@@ -1,7 +1,7 @@
+package edu.gatech.cs3240.driver;
 
 public class ParserGenerator {
     // TODO
-    
     public static void main(String[] args) {
 	System.out.println("Hello world!");
 	System.out.println("Right now, I don't do anything.");

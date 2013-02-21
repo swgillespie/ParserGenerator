b25829ffd457ff9dbd5006e513ab9558f65966dd
Sean Gillespie <sean@ubuntu.(none)>
2013-02-20 21:01:56 -0800

    Initial commit of all machinery required to build our project. I'll do a write-up about how to use it on the command line on the wiki, but eclipse supports it by default

---
+++ src/ParserGenerator.java
@@ -0,0 +1,9 @@
+
+public class ParserGenerator {
+    // TODO
+    
+    public static void main(String[] args) {
+	System.out.println("Hello world!");
+	System.out.println("Right now, I don't do anything.");
+    }
+}

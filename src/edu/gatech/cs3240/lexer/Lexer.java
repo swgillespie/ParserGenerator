dbef1272de21c789229f3fc2a526659b5cac31da
swgillespie <sean.william.g@gmail.com>
2013-02-26 17:10:41 -0500

    Clean up Lexer abstract class

--- src/edu/gatech/cs3240/lexer/Lexer.java
+++ src/edu/gatech/cs3240/lexer/Lexer.java
@@ -5,31 +5,44 @@ import java.util.Scanner;
 
 public abstract class Lexer {
 	
-	
-	private static final String EOF = "EOF";
+	private static char EOF = 0;
 	private Scanner scanner;
 	
 	/**
-	 * Constructs a new GrammarLexer that tokenizes an input stream according
-	 * to the context-free grammar given to us.
-	 * @param fileName The name of a file to tokenize
+	 * Constructs a new Lexer that lexes an input stream according
+	 * to a context-free grammar.
+	 * @param fileName The name of a file to lexify.
 	 * @throws LexerException Thrown if fileName doesn't describe a 
-	 * valid file
+	 * that exists
 	 */
 	public Lexer(String fileName) throws LexerException {
-		try {
-			scanner = new Scanner(new FileInputStream(new File(fileName)));
-		} catch (FileNotFoundException f) {
+		File inputFile = new File(fileName);
+		if (!inputFile.exists()) {
 			throw new LexerException("File not found: " + fileName);
+		}
+		try {
+			scanner = new Scanner(new FileInputStream(inputFile));
+		} catch (FileNotFoundException f) {
+			// this should never happen
 		}
 	}
 	
-	protected String nextString() {
-		if (scanner.hasNext()) {
-			return scanner.next();
+	/**
+	 * Returns the next byte available to the input stream, and 0 if there are no
+	 * more bytes.
+	 * @return The next character in the input stream, 0 if there are no more characters
+	 * available to the stream 
+	 */
+	protected char next() {
+		if (scanner.hasNextByte()) {
+			return (char)scanner.nextByte();
 		}
 		return EOF;
 	}
 	
+	/**
+	 * Lexes an input according to an implementation-defined grammar. Throws LexerExceptions
+	 * if syntax errors are encountered, and returns normally if no errors are found.
+	 */
 	public abstract void parse();
 }

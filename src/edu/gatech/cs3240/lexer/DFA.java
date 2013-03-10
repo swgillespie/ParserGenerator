bdf30df11ae2f273cac7eb42630cb5cea8d73ed9
Nina <ninaflaherty@gmail.com>
2013-03-10 10:55:01 -0400

    DFA creates an NFA from existing DFA - pretty sure it's working

--- src/edu/gatech/cs3240/lexer/DFA.java
+++ src/edu/gatech/cs3240/lexer/DFA.java
@@ -1,8 +1,8 @@
 package edu.gatech.cs3240.lexer;
 
+
 import java.util.*;
 
-//note: need to check if resulting state from transition is an existing state and if so loop back on that char
 
 /*
  * Creates a DFA from an existing NFA
@@ -10,36 +10,97 @@ import java.util.*;
 
 public class DFA extends NFA{
 	
-	//public NFA nfa;
-	public State start;
+	public State startState;
 	public ArrayList<Integer> alphabet;
 	private ArrayList<State> emptyTrans;
+	private ArrayList<State> currStateSet;
 	private ArrayList<State> stateSet;
+	private ArrayList<State> sSet;
 	private ArrayList<State> transSet;
-	private ArrayList<State> moveSet;
+	private ArrayList<State> tSet;
+	private State newState;
+	private Hashtable<State, ArrayList<State>> pairs1; 
+	private Hashtable<ArrayList<State>, State> pairs2;
+	private boolean accept;
+	private State currState;
 	
 	
-	public ArrayList<ArrayList<State>> dfa; //DFA represented as array list of array list where nested array list holds states
+	public ArrayList<State> dfa; //DFA represented as array list of array list where nested array list holds states
 	
 	public DFA(NFA from){
-		this.start = from.start; 
-		dfa = new ArrayList<ArrayList<State>>();
-		makeAlpha(from);
+		dfa = new ArrayList<State>();
+		pairs1 = new Hashtable<State,ArrayList<State>>();
+		pairs2 = new Hashtable<ArrayList<State>, State>();
+		accept = false;
+		create(from);
+		System.out.println("*****************dfa: "+dfa);
 	}
 	
-	/*
-	 * creates alphabet of all possible transition chars for DFA conversion
-	 */
-	public void makeAlpha(NFA n){
-		alphabet = new ArrayList<Integer>(); 
-		for(int i=0; i<n.start.getTrans().length; i++){
-			alphabet.add(i+32); //+32 representing chars ascii value
-		}
-	}
 	
 	public void create(NFA nfa){
-		epClosure(nfa.start); // get all 
+		int count = 0;
+		ArrayList<State> startStateSet = epClosure(nfa.start);
+		for(State s : startStateSet){
+			if(s.getAccept()){
+				accept = true;
+			}
 		}
+		State dfaStart = new State(accept);
+		pairs1.put(dfaStart, startStateSet);
+		pairs2.put(startStateSet, dfaStart);
+		dfa.add(dfaStart);
+		
+		for(State s : dfa){ //check if there are any unmarked states
+			if(!s.marked){
+				count++;
+				currState = s;
+				break;
+			}
+		}
+		
+		while(count > 0){
+			accept = false;
+			currState.setMarked(true);
+			currStateSet = pairs1.get(currState);
+			
+			for(int c=0;c<96;c++){
+				tSet = transition(currStateSet,c);
+				if(tSet.isEmpty()){
+					sSet = tSet; //considering null states
+				}
+				else{
+					sSet = epClosure(tSet);
+				}
+			
+				if(!pairs1.containsValue(sSet)){
+					for(State s : sSet){
+						if(s.getAccept()){
+							accept = true;
+						}
+					}
+					newState = new State(accept);
+					dfa.add(newState);
+					pairs1.put(newState, sSet);
+					pairs2.put(sSet, newState);
+				}
+				else{
+					newState = pairs2.get(sSet);
+				}
+				
+				currState.setTrans(c, newState);
+			}
+			
+			count = 0;
+			for(State s : dfa){ //check if there are any unmarked states
+				if(!s.marked){
+					count++;
+					currState = s; //update currstate
+					break;
+				}
+			}	
+		}//end while
+	}//end create
+
 	
 	/*
 	 * Returns set of NFA states reachable from an NFA state on empty transitions
@@ -64,6 +125,7 @@ public class DFA extends NFA{
 		return emptyTrans;
 	}
 
+	
 	/*
 	 * Returns a DFA state represented as a set of NFA states reachable 
 	 * from a set of states on empty transitions
@@ -72,9 +134,14 @@ public class DFA extends NFA{
 		stateSet = new ArrayList<State>();
 		for(State s : set){
 			ArrayList<State> subSet = epClosure(s);
+
+			
 			for(State state : subSet){
-				stateSet.add(state);
+				if(!stateSet.contains(state)){
+					stateSet.add(state);
+				}
 			}
+
 		}
 		return stateSet;
 	}
@@ -84,43 +151,18 @@ public class DFA extends NFA{
 	 * input set represents a DFA state made from combined NFA states
 	 */
 	public ArrayList<State> transition(ArrayList<State> set, Integer c){
-		//c = ascii value of char
 		transSet = new ArrayList<State>();
 		for(State s : set){
-			if(!s.getTrans()[c-32].isEmpty()){
-				for(State state : s.getTrans()[c-32]){
-					transSet.add(state);
+			if(!s.getTrans()[c].isEmpty()){
+				for(State state : s.getTrans()[c]){
+					if(!transSet.contains(state)){ //prevent adding same state twice
+						transSet.add(state);
+					}
 				}
 			}
-			//ArrayList<State> subSet = move(s, c-32);
-			/*for(State state : subSet){
-				transSet.add(state);
-			}*/
 		}
 		return transSet;
 	}
 	
-	/*public ArrayList<State> move(State state, Integer i){
-		//i = c-32
-		Stack<State> stack = new Stack<State>();
-		moveSet = new ArrayList<State>();
-		//moveSet.add(state);  only add next states reachable on i to moveSet, not state -- different from e transitions
-		for(State s : state.getTrans()[i]){
-			stack.push(s); // add states to stack
-			moveSet.add(s);
-		}
-		while(!stack.isEmpty()){ //if the stack isn't empty
-			State s = stack.pop();
-			for(State st : s.getTrans()[i]){
-				stack.push(st);
-				moveSet.add(st);
-			}
-		}
-		return moveSet;
-	}*/
 	
-	
-	
-	
-
 }
